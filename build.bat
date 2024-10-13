a868ce56485b59666a9750a9adb6b47c7c04d717
j-palomo <palomo.john@hotmail.com>
2024-10-12 20:33:36 -0400

    Compile acs before building, and check if it succeeds. Check for missing directory before compiling.

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
-7z a -tzip "D2NewGamePlus.pk3" *.txt* *.zscript* acs\ brightmaps\ graphics\ scripts\ zscript\ LICENSE
+IF NOT EXIST "acs\" md acs
+acc scripts\D2_NGPLUS.acs acs\D2_NGPLUS.o
+IF %ERRORLEVEL% EQU 0 ( 
+   7z a -tzip "D2NewGamePlus.pk3" *.txt* *.zscript* acs\ brightmaps\ graphics\ scripts\ zscript\ LICENSE
+)
--- compile_acs.bat
+++ compile_acs.bat
@@ -1,2 +1,2 @@
-md acs
+IF NOT EXIST "acs\" md acs
 acc scripts\D2_NGPLUS.acs acs\D2_NGPLUS.o
