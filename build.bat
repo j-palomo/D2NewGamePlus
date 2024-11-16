e857c0fdde39038801f797fc526c3fa667a9085d
j-palomo <palomo.john@hotmail.com>
2024-11-16 16:38:07 -0500

    Added custom texture for the cameras to use as the screen.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 IF NOT EXIST "acs\" md acs
 acc scripts\D2_NGPLUS.acs acs\D2_NGPLUS.o
 IF %ERRORLEVEL% EQU 0 ( 
-   7z a -tzip "D2NewGamePlus.pk3" *.txt* *.zmap *.zscript* acs\ brightmaps\ graphics\ scripts\ zscript\ LICENSE
+   7z a -tzip "D2NewGamePlus.pk3" *.txt* *.zmap *.zscript* acs\ brightmaps\ graphics\ scripts\ textures\ zscript\ LICENSE
 )
