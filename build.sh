4463049e79b32803eb03b9cd892cedf3516da553
John Palomo Jr. <palomo.john@hotmail.com>
2025-07-13 14:18:09 -0400

    Add support for building on Linux. Requires ACC to be placed in base directory.

---
+++ build.sh
@@ -0,0 +1,9 @@
+#! bin/bash
+mkdir -m 755 -p ./Levels/acs ./VFX/acs
+cd ./Levels/scripts
+../../acc ./D2_NGPLUS.acs ../acs/D2_NGPLUS.o
+../../acc ../../VFX/scripts/FX_D2_NGPLUS.acs ../../VFX/acs/FX_D2_NGPLUS.o
+cd ../..
+zip -v -r D2NewGamePlus_Levels.pk3 Levels/
+zip -v -r D2NewGamePlus_VFX.pk3 VFX/
+zip -v -r D2NewGamePlus_Weapons.pk3 Weapons/
